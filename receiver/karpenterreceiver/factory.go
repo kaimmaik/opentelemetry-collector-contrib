4f3eeca94733f535847a6e4fa5039f0ee970ff73
medamink <medamink@amazon.com>
2025-07-25 12:25:03 +0000

    Add wrapper receivers for KEDA and Karpenter

--- receiver/karpenterreceiver/factory.go
+++ receiver/karpenterreceiver/factory.go
@@ -7,11 +7,19 @@ import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/receiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/prometheusreceiver"
+	promconfig "github.com/prometheus/prometheus/config"
+	"github.com/prometheus/common/model"
 )
 
 const (
-	typeStr = "karpenter"
+	typeStr         = "karpenterreceiver"
+	defaultEndpoint = "http://karpenter.karpenter.svc.cluster.local:8000/metrics"
 )
+
+type Config struct {
+	prometheusreceiver.Config `mapstructure:",squash"`
+}
 
 func NewFactory() receiver.Factory {
 	return receiver.NewFactory(
@@ -22,17 +30,37 @@ func NewFactory() receiver.Factory {
 }
 
 func createDefaultConfig() component.Config {
-	return &Config{
-		Endpoint:       "http://karpenter.karpenter.svc.cluster.local:8000/metrics",
-		ScrapeInterval: 30 * time.Second,
+	promFactory := prometheusreceiver.NewFactory()
+	promCfg := promFactory.CreateDefaultConfig().(*prometheusreceiver.Config)
+	
+	// Set up default Karpenter scrape config
+	promCfg.PrometheusConfig.ScrapeConfigs = []*promconfig.ScrapeConfig{
+		{
+			JobName:        "karpenter-metrics",
+			ScrapeInterval: model.Duration(30 * time.Second),
+			ScrapeTimeout:  model.Duration(10 * time.Second),
+			MetricsPath:    "/metrics",
+			Scheme:         "http",
+			StaticConfigs: []*promconfig.Group{
+				{
+					Targets: []model.LabelSet{
+						{model.AddressLabel: model.LabelValue(defaultEndpoint)},
+					},
+				},
+			},
+		},
 	}
+	
+	return &Config{Config: *promCfg}
 }
 
 func createMetricsReceiver(
-	_ context.Context,
+	ctx context.Context,
 	set receiver.Settings,
 	cfg component.Config,
 	nextConsumer consumer.Metrics,
 ) (receiver.Metrics, error) {
-	return newKarpenterReceiver(set, cfg.(*Config), nextConsumer)
+	karpenterCfg := cfg.(*Config)
+	promFactory := prometheusreceiver.NewFactory()
+	return promFactory.CreateMetricsReceiver(ctx, set, &karpenterCfg.Config, nextConsumer)
 }
--- receiver/kedareceiver/factory.go
+++ receiver/kedareceiver/factory.go
@@ -7,11 +7,19 @@ import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/receiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/prometheusreceiver"
+	promconfig "github.com/prometheus/prometheus/config"
+	"github.com/prometheus/common/model"
 )
 
 const (
-	typeStr = "keda"
+	typeStr         = "kedareceiver"
+	defaultEndpoint = "http://keda-operator.keda-system.svc.cluster.local:8080/metrics"
 )
+
+type Config struct {
+	prometheusreceiver.Config `mapstructure:",squash"`
+}
 
 func NewFactory() receiver.Factory {
 	return receiver.NewFactory(
@@ -22,17 +30,37 @@ func NewFactory() receiver.Factory {
 }
 
 func createDefaultConfig() component.Config {
-	return &Config{
-		Endpoint:       "http://keda-operator.keda-system.svc.cluster.local:8080/metrics",
-		ScrapeInterval: 30 * time.Second,
+	promFactory := prometheusreceiver.NewFactory()
+	promCfg := promFactory.CreateDefaultConfig().(*prometheusreceiver.Config)
+	
+	// Set up default KEDA scrape config
+	promCfg.PrometheusConfig.ScrapeConfigs = []*promconfig.ScrapeConfig{
+		{
+			JobName:        "keda-metrics",
+			ScrapeInterval: model.Duration(30 * time.Second),
+			ScrapeTimeout:  model.Duration(10 * time.Second),
+			MetricsPath:    "/metrics",
+			Scheme:         "http",
+			StaticConfigs: []*promconfig.Group{
+				{
+					Targets: []model.LabelSet{
+						{model.AddressLabel: model.LabelValue(defaultEndpoint)},
+					},
+				},
+			},
+		},
 	}
+	
+	return &Config{Config: *promCfg}
 }
 
 func createMetricsReceiver(
-	_ context.Context,
+	ctx context.Context,
 	set receiver.Settings,
 	cfg component.Config,
 	nextConsumer consumer.Metrics,
 ) (receiver.Metrics, error) {
-	return newKedaReceiver(set, cfg.(*Config), nextConsumer)
+	kedaCfg := cfg.(*Config)
+	promFactory := prometheusreceiver.NewFactory()
+	return promFactory.CreateMetricsReceiver(ctx, set, &kedaCfg.Config, nextConsumer)
 }
